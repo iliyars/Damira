5a2921ff161e3f709764962583898cff9f4d0321
Ilya Belan <iliyars97@gmail.com>
2025-05-08 23:06:39 +0300

    update

--- aerospace matlab/MaaWM/README.md
+++
@@ -1,3 +0,0 @@
-This is the Multi Aircraft Atmospheric Windmapper Simulation Tool
-
-Explanation of the model in detail can be found in the Documentation Folder
--- aerospace matlab/rockets/Launch_Society/CPB_SERIAL_HANDSHAKE/microcontroller_side.py
+++
@@ -1,9 +0,0 @@
-import time
-import supervisor
-
-while True:
-	if supervisor.runtime.serial_bytes_available:
-		df = input.strip()
-		print('Received df command = ',df)
-		#Tell the servo to move
-	time.sleep(0.01)
